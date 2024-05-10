e995c10c4265d95e4fb757e7e74a8463974fe79c
Rakib-Hasan25 <you@example.com>
2024-05-11 01:38:22 +0600

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,41 @@
+//!type : 01
+let score ="10"
+console.log(typeof(score))//String
+
+let valueInNumber = Number(score)//convert to number
+
+
+//!type : 02
+let score2="10aa"
+let valueInNumber2 = Number(score)//convert to number 
+console.log(valueInNumber2)//"NaN" -> not a number
+
+
+//!type : 03
+
+console.log(Number(null))//give 0 
+
+
+//!type : 04
+
+console.log(Number(true))//give 1
+
+
+
+//!type:05
+
+let isLoggedIn="rakib"
+let booleanInLoggedIn = Boolean(isLoggedIn) //true
+
+let isLoggedIn2=""
+let booleanInLoggedIn2 = Boolean(isLoggedIn2) //false
+
+
+//!type:06
+let someNumber =33
+let stringNumber = String(someNumber) //"33"
+
+
+
+
+
